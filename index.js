f035a92a1e74548a3e145ff4e993bd21aa450a7a
etozhenerk <etozhenerk@yandex.ru>
2020-12-20 16:05:50 +0300

    Lesson 14 дополнительное задание

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
 "use strict";
-
-function DomElement(selector, height, width, bg, fontSize) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
-}
-DomElement.prototype.create = function () {
-  if (this.selector[0] === "#") {
-    let block = document.createElement("p");
-    block.id = this.selector.slice(1);
-    block.style.cssText = `
-    height: ${this.height};
-    width: ${this.width};
-    background: ${this.bg};
-    font-size: ${this.fontSize};`;
-    block.textContent =
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dolorem hic incidunt! Non, dignissimos iusto sit aperiam neque vitae ad illum praesentium provident laborum excepturi ex dicta explicabo cum fugiat?";
-    document.querySelector("body").append(block);
-  } else if (this.selector[0] === ".") {
-    let block = document.createElement("div");
-    block.style.cssText = `
-    height: ${this.height};
-    width: ${this.width};
-    background: ${this.bg};
-    font-size: ${this.fontSize};`;
-    block.classList.add(this.selector.slice(1));
-    block.textContent =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dolorem hic incidunt! Non, dignissimos iusto sit aperiam neque vitae ad illum praesentium provident laborum excepturi ex dicta explicabo cum fugiat?";
-    document.querySelector("body").append(block);
+window.addEventListener("DOMContentLoaded", () => {
+  function DomElement(selector, height, width, bg, fontSize, top = 0, left = 0) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.top = top;
+    this.left = left;
+    this.fontSize = fontSize;
   }
-};
+  DomElement.prototype.create = function () {
+    if (this.selector[0] === "#") {
+      let block = document.createElement("p");
+      block.id = this.selector.slice(1);
+      block.style.cssText = `
+      position: fixed;
+      margon: 0;
+      transition: all 1s;
+      height: ${this.height}px;
+      width: ${this.width}px;
+      background: ${this.bg};
+      font-size: ${this.fontSize};`;
+      document.querySelector("body").append(block);
+    } else if (this.selector[0] === ".") {
+      let block = document.createElement("div");
+      block.style.cssText = `
+      position: fixed;
+      transition: all 1s;
+      height: ${this.height}px;
+      width: ${this.width}px;
+      background: ${this.bg};
+      font-size: ${this.fontSize};`;
+      block.classList.add(this.selector.slice(1));
+      document.querySelector("body").append(block);
+    }
+  };
 
-let elem = new DomElement("#class", "20%", "80%", "url('https://mdn.mozillademos.org/files/11983/starsolid.gif') #00D repeat-y fixed", "120px");
-console.log(elem);
-elem.create();
+  DomElement.prototype.move = function (e) {
+    const elem = document.querySelector(this.selector);
+    let key = e.key;
+    if (key === "ArrowUp") {
+      this.top -= 10;
+    } else if (key === "ArrowDown") {
+      this.top += 10;
+    } else if (key === "ArrowLeft") {
+      this.left -= 10;
+    } else if (key === "ArrowRight") {
+      this.left += 10;
+    }
+    elem.style.top = this.top + "px";
+    elem.style.left = this.left + "px";
+  };
+
+  let elem = new DomElement(".class", "100", "100", "#00D", "30px");
+  elem.create();
+
+  document.addEventListener("keydown", elem.move.bind(elem));
+});
